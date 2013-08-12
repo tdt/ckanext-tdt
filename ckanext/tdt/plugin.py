39f344f2f8287faa0db86dd154a55fd32ec38370
Pieter Colpaert <pieter@iRail.be>
2013-08-12 16:52:35 +0200

    adds XML files to a The  DataTank instance

--- ckanext/tdt/plugin.py
+++ ckanext/tdt/plugin.py
@@ -6,8 +6,6 @@ from logging import getLogger
 
 log = getLogger(__name__)
 
-log.info("Loading TDT Plugin class")
-
 class TDTPlugin(p.SingletonPlugin):
     """This plugin hooks in when a data package has been added.
        The resulting package will be investigated for The DataTank readable sources.
@@ -28,9 +26,11 @@ class TDTPlugin(p.SingletonPlugin):
         """This method should add a resource configuration to The DataTank and return the uri
         """
         log.info(entity)
-        if(hasattr(entity, 'format') and entity.format.lower() == "itsps"):
-            log.info("We have an ITS PS FILE! Let's do a request!")
-            r = requests.put("http://localhost/test", data="testing - check access.log afterwards")
-            log.info(r.status_code)
+        if(hasattr(entity, 'format') and entity.format.lower() == "xml"):
+            log.info("We have an XML FILE! Let's do a request!")
+            # !! name is optional !!
+            r = requests.put("http://{TDTINSTANCE}/tdtadmin/resources/ckan/" + entity.name, auth=('USERNAME', 'PASSWORD'), data="resource_type=generic&generic_type=XML&documentation=" + entity.description +"&uri=" + entity.url)
+            log.info(r.headers["content-location"])
+            
 
         return
