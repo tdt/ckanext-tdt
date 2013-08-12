66d6eb2557e093433ca2fcc2c9348510fb8e7da3
Pieter Colpaert <pieter@iRail.be>
2013-08-12 10:53:38 +0200

    Initialize CKAN extention repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# An extension for CKAN
+
+CKAN is a metadata platform that enables organisations to publish datasets.
+
+This extension is a hook for each time when a dataset is added, when that dataset is proxyable through The DataTank, CKAN will automatically change the URL with a The DataTank adapter and deliver the data in XML, JSON and CSV. 
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+	name='ckanext-tdt',
+	version=version,
+	description="A The DataTank extension for CKAN",
+	long_description="""\
+	""",
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='',
+	author='Pieter Colpaert',
+	author_email='pieter.colpaert@okfn.org',
+	url='http://thedatatank.com',
+	license='AGPLv3',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	namespace_packages=['ckanext', 'ckanext.tdt'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		# -*- Extra requirements: -*-
+	],
+	entry_points=\
+	"""
+        [ckan.plugins]
+	# Add plugins here, eg
+	# myplugin=ckanext.tdt:PluginClass
+	""",
+)
