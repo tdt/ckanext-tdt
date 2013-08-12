5237f9f0786270106e2e98ffb9323218b28437f8
Pieter Colpaert <pieter@iRail.be>
2013-08-12 15:04:24 +0200

    Sending a PUT request to an instance whenever a dataset is updated or added

--- ckanext/tdt/plugin.py
+++ ckanext/tdt/plugin.py
@@ -1,11 +1,8 @@
-import os
+import requests
 import ckan.plugins as p
+from ckan import model
 
 from logging import getLogger
-
-from pylons import request
-
-
 
 log = getLogger(__name__)
 
@@ -16,30 +13,24 @@ class TDTPlugin(p.SingletonPlugin):
        The resulting package will be investigated for The DataTank readable sources.
        When a match has been found, the dataset will be added to The DataTank and the URI in CKAN will change
     """
-    p.implements(p.IPackageController, inherit=True)
 
-    def after_create(self, context, pkg_dict):
-        """This method is called upon after a dataset package has been created.
-           It should also be created in The DataTank
+    # inheriting from IDomainObjectModification makes sure that we get notifications about updates with resources
+    p.implements(p.IDomainObjectModification)
+
+    def notify(self, entity, operation=None):
+        # Make sure we're working with a Resource
+        if isinstance(entity, model.Resource):
+            self.create_tdt_source(entity)
+        return
+
+
+    def create_tdt_source(self, entity):
+        """This method should add a resource configuration to The DataTank and return the uri
         """
-        log.info("Package has been created")
-        ## STUB
+        log.info(entity)
+        if(hasattr(entity, 'format') and entity.format.lower() == "itsps"):
+            log.info("We have an ITS PS FILE! Let's do a request!")
+            r = requests.put("http://localhost/test", data="testing - check access.log afterwards")
+            log.info(r.status_code)
 
-        return 
-
-    def after_update(self, context, pkg_dict):
-        """This method is called upon after a dataset package has been updated.
-           It should also be updated in The DataTank
-        """
-        log.info("Package has been updated")
-        ## STUB
-        return  
-
-
-    def after_delete(self, context, pkg_dict):
-        """This method is called upon after a dataset package has been delete.
-           It should also be deleted in The DataTank
-        """
-        log.info("Package has been deleted")
-        ## STUB
-        return 
+        return
