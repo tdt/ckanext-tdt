9e0fe7208db3d2dc42bcf28ddf389c08b53964f4
Pieter Colpaert <pieter@iRail.be>
2013-08-12 14:01:09 +0200

    inherit from an interface before overriding the functions

--- ckanext/tdt/plugin.py
+++ ckanext/tdt/plugin.py
@@ -1,19 +1,22 @@
 import os
+import ckan.plugins as p
+
 from logging import getLogger
 
 from pylons import request
 
-from ckan.plugins import implements, SingletonPlugin
+
 
 log = getLogger(__name__)
 
 log.info("Loading TDT Plugin class")
 
-class TDTPlugin(SingletonPlugin):
+class TDTPlugin(p.SingletonPlugin):
     """This plugin hooks in when a data package has been added.
        The resulting package will be investigated for The DataTank readable sources.
        When a match has been found, the dataset will be added to The DataTank and the URI in CKAN will change
     """
+    p.implements(p.IPackageController, inherit=True)
 
     def after_create(self, context, pkg_dict):
         """This method is called upon after a dataset package has been created.
